2f27f20fee7a5664c062a3bc16e9b87dfce735c1
Deepshikha1993 <71941483+Deepshikha1993@users.noreply.github.com>
2020-09-27 11:01:53 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import RegisterForm from './registrationForm/RegisterForm';
+
+
+ReactDOM.render(<RegisterForm /> , document.getElementById('root'));
+
+registerServiceWorker();
+
+class RegisterForm extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        fields: {},
+        errors: {}
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+      this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
+
+    };
+
+    handleChange(e) {
+      let fields = this.state.fields;
+      fields[e.target.name] = e.target.value;
+      this.setState({
+        fields
+      });
+
+    }
+
+    submituserRegistrationForm(e) {
+      e.preventDefault();
+      if (this.validateForm()) {
+          let fields = {};
+          fields["username"] = "";
+          fields["password"] = "";
+          this.setState({fields:fields});
+          alert("Form submitted");
+      }
+
+    }
+
+    validateForm() {
+
+      let fields = this.state.fields;
+      let errors = {};
+      let formIsValid = true;
+
+      if (!fields["username"]) {
+        formIsValid = false;
+        errors["username"] = "*Please enter your username.";
+      }
+
+      if (typeof fields["username"] !== "undefined") {
+        if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+          formIsValid = false;
+          errors["username"] = "*Please enter alphabet characters only.";
+        }
+      }
+
+      if (!fields["emailid"]) {
+        formIsValid = false;
+        errors["emailid"] = "*Please enter your email-ID.";
+      }
+
+      if (typeof fields["emailid"] !== "undefined") {
+        //regular expression for email validation
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(fields["emailid"])) {
+          formIsValid = false;
+          errors["emailid"] = "*Please enter valid email-ID.";
+        }
+      }
+
+     
+      
+
+      if (!fields["password"]) {
+        formIsValid = false;
+        errors["password"] = "*Please enter your password.";
+      }
+
+      if (typeof fields["password"] !== "undefined") {
+        if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+          formIsValid = false;
+          errors["password"] = "*Please enter secure and strong password.";
+        }
+      }
+
+      this.setState({
+        errors: errors
+      });
+      return formIsValid;
+
+
+    }
+
+
+
+  render() {
+    return (
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+ 
+ <form class="form-horizontal" action="/Desktopview.html">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="username">User Name:</label>
+      <div class="col-sm-6">
+        <input type="username" class="form-control" id="email" placeholder="Enter your username" name="username" >
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-6">          
+        <input type="password" class="form-control" id="pwd" placeholder="Enter  your password" name="pwd" >
+      </div>
+    </div>
+    
+    
+   <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+        <button type="Submit" class="btn btn-default">Reset</button>
+     
+        <button type="Submit" class="btn btn-default">Login</button>
+     </div>
+     </div>
+</form>
+
+    );
+  }
+}
+
+export default RegisterForm;
